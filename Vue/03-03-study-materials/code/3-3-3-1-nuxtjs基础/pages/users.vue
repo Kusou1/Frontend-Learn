1aa503d400ce8107a0dacaf4ee883a1c178c059b
zhang <zhangshaohong1997@gmail.com>
2021-04-03 14:33:08 +0800

    nuxt基础

---
+++ Vue/03-03-study-materials/code/3-3-3-1-nuxtjs基础/pages/users.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <h1>Users 父路由</h1>
+      <nuxt-child></nuxt-child>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    
+  },
+  computed: {
+    
+  },
+  watch: {
+    
+  }
+}
+</script>
+<style scoped>
+</style>
---
+++ Vue/03-03-study-materials/code/3-3-3-1-nuxtjs基础/pages/users/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <h1>User 子路由</h1>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    
+  },
+  computed: {
+    
+  },
+  watch: {
+    
+  }
+}
+</script>
+<style scoped>
+</style>

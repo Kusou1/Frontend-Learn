33bba940d1d9a1f81156f8401317189baa6848c7
zhang <zhangshaohong1997@gmail.com>
2021-04-29 19:46:49 +0800

    strapi

---
+++ Vue/03-04-study-materials/code/my-project/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1f20d0ada7f07ab368eb4b0c622edf77'),
+    },
+  },
+});
---
+++ Vue/03-04-study-materials/code/my-project/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '01e22c82-ca95-4932-b591-55d0a6c7d97e'
+};

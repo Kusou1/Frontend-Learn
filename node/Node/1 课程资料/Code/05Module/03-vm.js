40143e6966399a7285d73f194d9cce8ab05ac022
zhangshaohong <zhangshaohong@yizhicloud.cn>
2022-04-11 17:54:21 +0800

    vm模块

--- node/Node/1 课程资料/Code/05Module/03-vm.js
+++ node/Node/1 课程资料/Code/05Module/03-vm.js
@@ -4,14 +4,18 @@ const vm = require('vm')
 let age = 33
 let content = fs.readFileSync('test.txt', 'utf-8')
 
-// eval
+// eval 直接用eval执行的话是不合适的，因为如果在这个文件中已定义同名变量，就会报错，因为他们用的是同一个上下文
 // eval(content)
 
-// new Function
+// new Function 虽然可以执行，但是比eval还要麻烦,还要考虑函数怎么去定义，只有一个变量还行，有多个的话就很麻烦
 /* console.log(age)
 let fn = new Function('age', "return age + 1")
 console.log(fn(age)) */
 
+
+// 基于这样的场景，在node底层都没有采用，而是使用vm的机制
+ 
+// 沙箱的运行api
 vm.runInThisContext("age += 10")
 
 console.log(age)

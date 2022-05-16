61b4585f28d31d60e9ccd5995f5f089a6c025f0c
Kusou1 <zhangshaohong1997@gmail.com>
2022-05-16 20:57:58 +0800

    root

--- react/Redux/react-redux-guide/src/store/sagas/root.saga.js
+++ react/Redux/react-redux-guide/src/store/sagas/root.saga.js
@@ -3,6 +3,7 @@ import counterSaga from './counter.saga';
 import modalSaga from './modal.saga';
 
 export default function* rootSaga () {
+  // 合并sage模块
   yield all([
     counterSaga(),
     modalSaga()

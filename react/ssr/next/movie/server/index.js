7ea76bbf68895bd51364db5339a513baa9d75978
Kusou1 <zhangshaohong1997@gmail.com>
2022-05-23 22:10:07 +0800

    next

--- react/ssr/next/movie/server/index.js
+++ react/ssr/next/movie/server/index.js
@@ -1,3 +1,4 @@
+// 自定义Next应用服务器
 const express = require('express');
 const next = require('next');
 
@@ -5,8 +6,10 @@ const dev = process.env.NODE_ENV !== 'production'
 
 const app = next({dev});
 
+// 获取next提供的路由系统
 const handler = app.getRequestHandler();
 
+// perpare方法表示当next应用准备好之后，就可以去自定义内容
 app.prepare().then(() => {
   const server = express();
   server.get('/hello', (req, res) => {

dea776f9c385bd800788f95a2e34515bf2be18ab
Kusou1 <zhangshaohong1997@gmail.com>
2022-07-18 21:47:44 +0800

    web component

---
+++ WebComponent/lifeCycle/component.vue
@@ -0,0 +1,17 @@
+<template></template>
+
+<script>
+export default {
+    setup() {},
+    beforeCreate() {},
+    beforeMount(){},
+    mounted(){},//相当于connectedCallback
+    beforeUpdate(){},
+    Updated(){},
+    beforeUnmount(){},
+    unMounted(){}//相当于disconnectedCallback
+};
+</script>
+
+<style>
+</style>

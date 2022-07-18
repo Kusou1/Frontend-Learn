f9e5f803aa517e56c03892aee6073a9e15a204d5
Kusou1 <zhangshaohong1997@gmail.com>
2022-07-18 23:17:17 +0800

    web component lifeCycle

--- WebComponent/lifeCycle/component.vue
+++ WebComponent/lifeCycle/component.vue
@@ -1,15 +1,30 @@
-<template></template>
+<template>
+    <h1 :style="{ color }">Vue</h1>
+</template>
 
 <script>
 export default {
+    // 类似observedAttributes
+    data() {
+        return {
+            color: "purple",
+        };
+    },
     setup() {},
     beforeCreate() {},
-    beforeMount(){},
-    mounted(){},//相当于connectedCallback
-    beforeUpdate(){},
-    Updated(){},
-    beforeUnmount(){},
-    unMounted(){}//相当于disconnectedCallback
+    beforeMount() {},
+    mounted() {
+        this.color = 'red'
+    }, //相当于connectedCallback
+    beforeUpdate() {},
+    Updated() {},
+    beforeUnmount() {},
+    unMounted() {}, //相当于disconnectedCallback
+    watch: {
+        color(newValue, oldValue) {
+            this.$refs.dom.style.color = newValue;
+        },
+    },
 };
 </script>
 
